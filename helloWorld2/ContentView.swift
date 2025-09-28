730490629e1a67e44acdf3bf3520821633798e13
Amnah Albrahim <amnahalbrahim@10-101-41-11.0101.twq.sa.iosda.org>
2025-09-28 11:05:44 +0300

    Update greeting text

--- helloWorld2/ContentView.swift
+++ helloWorld2/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world My name is Amnah Albrahim")
+            Text("Apple Academy")
         }
         .padding()
     }
