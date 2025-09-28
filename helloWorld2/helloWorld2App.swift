47043283a04571a4c4fa5195862ced58c9ad380e
Amnah Albrahim <amnahalbrahim@10-101-41-11.0101.twq.sa.iosda.org>
2025-09-28 10:27:11 +0300

    Initial commit for helloWorld2

---
+++ helloWorld2/helloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorld2App.swift
+//  helloWorld2
+//
+//  Created by Amnah Albrahim on 06/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct helloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
